4a0335c95fd81763cf833f47f84e6d198083815f
pymedigitalcamaras <pymedigitalcamaras@gmail.com>
2026-06-02 16:50:52 -0400

    v8: fix CSS with output export

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,9 @@ const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    unoptimized: true,
   },
   trailingSlash: false,
 };
